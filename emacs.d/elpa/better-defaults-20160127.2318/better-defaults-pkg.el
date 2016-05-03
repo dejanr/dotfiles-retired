6675d258304015dbad23fcaaf5ff1f889d259898
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2016-05-03 15:00:01 +0200

    Add xdefaults, xsession and move emacs config to different folder

--- emacs.d/elpa/better-defaults-20160127.2318/better-defaults-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "better-defaults" "20160127.2318" "Fixing weird quirks and poor defaults" 'nil :url "https://github.com/technomancy/better-defaults" :keywords '("convenience"))
--- emacs.d/elpa/scpaste-20151208.1735/scpaste-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "scpaste" "20151208.1735" "Paste to the web via scp." '((htmlize "1.39")) :url "https://github.com/technomancy/scpaste" :keywords '("convenience" "hypermedia"))
