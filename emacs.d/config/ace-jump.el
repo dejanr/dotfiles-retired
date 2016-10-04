8d9db4cc22c6dbe05a1193517b7bd478e76233d5
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2016-10-05 01:10:51 +0200

    Remove old emacs

--- emacs.d/config/ace-jump.el
+++
@@ -1,6 +0,0 @@
-(use-package ace-jump-mode
-  :init
-  (progn
-    (global-set-key (kbd "C-c f") 'ace-jump-word-mode)
-    (global-set-key (kbd "C-c w") 'ace-jump-word-mode)
-    (global-set-key (kbd "C-x C-b") 'ace-jump-buffer)))
--- emacs.d/config/shell.el
+++
@@ -1,7 +0,0 @@
-(use-package shell
-  :init
-  (progn
-    (global-set-key (kbd "C-x C-t") 'ansi-term)
-    (setq explicit-shell-file-name "/run/current-system/sw/bin/bash")
-    (setq shell-file-name "/run/current-system/sw/bin/bash")
-    (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)))
