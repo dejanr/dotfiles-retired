2db29ffd2f0c8b55856c7a056fc151633ea2895f
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2017-01-05 16:31:04 +0100

    Remove custom nixpkgs, as i am not sure should i use git repo or not

--- install.sh
+++ install.sh
@@ -10,11 +10,6 @@ if [[ -d vim/bundle/vundle ]]; then
   curl -fLo vim/autoload/plug.vim https://raw.github.com/junegunn/vim-plug/master/plug.vim
 fi
 
-if [[ ! -d ~/.nix-defexpr ]]; then
-  mkdir -p ~/.nix-defexpr
-  link "$HOME/.dotfiles/nixpkgs" "$HOME/.nix-defexpr/nixpkgs"
-fi
-
 for location in $(find $HOME/.dotfiles -maxdepth 1 -name '*' ! -path '*.git'| sort); do
   name=$(basename $location)
   link "$HOME/.dotfiles/$name" "$HOME/.$name"
