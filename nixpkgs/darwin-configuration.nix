f607de8acd20e4c68bb9883822138bf785a2095a
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2017-11-06 01:37:19 +0100

    Darwin configurations

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -22,17 +22,51 @@
     pkgs.reattach-to-user-namespace
   ];
 
-  # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.bash.enable = true;
+  system.defaults.NSGlobalDomain.AppleKeyboardUIMode = 3;
+  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
+  system.defaults.NSGlobalDomain.InitialKeyRepeat = 10;
+  system.defaults.NSGlobalDomain.KeyRepeat = 1;
+  system.defaults.NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+  system.defaults.NSGlobalDomain.NSNavPanelExpandedStateForSaveMode = true;
+  system.defaults.NSGlobalDomain.NSNavPanelExpandedStateForSaveMode2 = true;
 
-  # Recreate /run/current-system symlink after boot.
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.orientation = "left";
+  system.defaults.dock.showhidden = true;
+  system.defaults.dock.mru-spaces = false;
+
+  system.defaults.finder.AppleShowAllExtensions = true;
+  system.defaults.finder.QuitMenuItem = true;
+  system.defaults.finder.FXEnableExtensionChangeWarning = false;
+
+  system.defaults.trackpad.Clicking = true;
+  system.defaults.trackpad.TrackpadThreeFingerDrag = true;
+
+  programs.bash.enable = true;
+  programs.nix-index.enable = true;
+
   services.activate-system.enable = true;
+  services.nix-daemon.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 2;
 
-  # You should generally set this to the total number of logical cores in your system.
-  # $ sysctl -n hw.ncpu
   nix.maxJobs = 4;
+  nix.package = pkgs.nixUnstable;
+  nix.extraOptions = ''
+    gc-keep-derivations = true
+    gc-keep-outputs = true
+  '';
+  nix.nixPath = [
+    # Use local nixpkgs checkout instead of channels.
+    "darwin-config=$HOME/.nixpkgs/darwin-configuration.nix"
+    "darwin=$HOME/.nix-defexpr/darwin"
+    "nixpkgs=$HOME/.nix-defexpr/nixpkgs"
+    "$HOME/.nix-defexpr/channels"
+  ];
 }
